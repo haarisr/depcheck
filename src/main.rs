58873a46fd8175571547081a3e5d714f9e02165f
Haaris Rahman <haarisrahman@gmail.com>
2025-10-24 01:42:07 -0700

    Read a file and parse it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(help = "Path to the file to process")]
+    path: String,
+}
+
+fn main() -> Result<()> {
+    let cli = Args::parse();
+    let contents = fs::read_to_string(&cli.path)?;
+    println!("Contents of the file");
+    println!("{}", contents);
+
+    Ok(())
 }
